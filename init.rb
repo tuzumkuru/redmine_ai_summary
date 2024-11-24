9edfb6c8564e8f9790a8768d93318adf97e6a124
Tolga Uzun <tolga.uzun@bamatekoloji.com>
2024-11-24 02:15:03 +0000

    Add plugin settings,  its view and translations

--- init.rb
+++ init.rb
@@ -3,6 +3,18 @@ Redmine::Plugin.register :redmine_ai_summary do
   author 'Tolga Uzun'
   description 'This is a plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/tuzumkuru/redmine_ai_summary'
+  author_url 'https://github.com/tuzumkuru'
+  requires_redmine '5.0'
+
+  settings default: {
+    'auto_generate' => false,
+    'api_address' => '',
+    'api_key' => ''
+  }, partial: 'settings/ai_summary_settings'
+  
+  # Add a permission for updating summaries
+  project_module :ai_summary do
+    permission :update_summaries, { :ai_summary => :update }
+  end
 end
