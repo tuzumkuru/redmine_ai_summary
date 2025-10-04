74029d49578ec38a840888a7a57880b1e9d40e6f
Tolga Uzun <tolgauzun@gmail.com>
2025-10-04 15:40:56 -0400

    fix: show updater and creator names

--- app/models/issue_summary.rb
+++ app/models/issue_summary.rb
@@ -1,7 +1,7 @@
 class IssueSummary < ActiveRecord::Base
-    belongs_to :issue
-    belongs_to :user, foreign_key: :created_by
-  
-    validates :summary, presence: true
-  end
-  
+  belongs_to :issue
+  belongs_to :creator, class_name: 'User', foreign_key: :created_by, optional: true
+  belongs_to :updater, class_name: 'User', foreign_key: :updated_by, optional: true
+
+  validates :summary, presence: true
+end
